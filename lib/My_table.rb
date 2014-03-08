82bb70c7d0616bd66d178dae6ce72d1e5a285d11
arts-webmaster <web.arts.unimelb@gmail.com>
2014-03-08 15:24:23 +1100

    able to insert record

--- lib/My_table.rb
+++ lib/My_table.rb
@@ -1,13 +1,33 @@
 # Class 
 # http://zetcode.com/db/mysqlrubytutorial/
 class My_table
-	def initialize
-
+	attr_accessor :host, :user, :pass, :db
+	
+	def initialize(host, user, pass, db)
+		@host = host
+		@user = user
+		@pass = pass
+		@db = db
 	end
 
 	def create
   	begin
-    	con = Mysql.new 'localhost', 'root', '11111111', 'asian_cinema'
+    	con = Mysql.new @host, @user, @pass, @db 
+		
+			con.query("
+        CREATE TABLE IF NOT EXISTS \
+        AacflmDirector(
+          local_id INT PRIMARY KEY AUTO_INCREMENT, 
+         	slug VARCHAR(255),
+					name VARCHAR(255),
+					bio_markup TEXT, 
+					created_on DATE,
+					page_id INT(11),
+					id INT(11),
+					bio INT(11),
+					site_id INT(11) 
+        )
+      ")
 
     	con.query("
       	CREATE TABLE IF NOT EXISTS \
@@ -31,4 +51,60 @@ class My_table
     	con.close if con
   	end
 	end
+
+
+	def insert_AacflmDirection(position, film_id, created_on, page_id, director_id, id, site_id)		
+		begin
+      con = Mysql.new @host, @user, @pass, @db
+
+		  stmt = con.prepare("
+				INSERT INTO \
+				AacflmDirection
+				(
+					position,
+					film_id,
+					created_on,
+					page_id,
+
+					director_id,
+					id,
+					site_id	
+				)	
+				VALUES
+				(
+					?,				
+					?,
+					?,
+					?,
+
+					?,
+					?,
+					?
+				)
+			")
+
+		  stmt.execute(
+				position,
+		    film_id,
+		    created_on,
+		    page_id,
+
+		    director_id,
+		    id,
+		    site_id	
+			)
+
+		rescue Mysql::Error => e
+      puts e.errno
+      puts e.error
+
+    ensure
+      con.close if con
+    end
+
+	end
+
+
 end
+
+
--- load.rb
+++ load.rb
@@ -7,7 +7,12 @@ require 'AacflmDirector'
 require 'My_table'
 
 #
-my_table = My_table.new
+host = 'localhost'
+user = 'root'
+pass = '11111111'
+db = 'asian_cinema'
+
+my_table = My_table.new host, user, pass, db 
 my_table.create
 
 
@@ -15,7 +20,17 @@ SITE_PATH = '/var/www/test/testme/asian_cinema/tmp_orig.yml'
 site_path = File.open( SITE_PATH )
 YAML::load_stream(site_path) { |doc|
 	if doc.class.to_s == 'AacflmDirection'
-		puts doc.attributes['position']
+		position =  doc.attributes['position']
+		film_id = doc.attributes['film_id']
+		created_on = doc.attributes['created_on']
+		page_id = doc.attributes['page_id']
+		
+		director_id = doc.attributes['director_id']
+		id = doc.attributes['id']
+		site_id = doc.attributes['site_id']
+		
+		my_table.insert_AacflmDirection position, film_id, created_on, page_id, director_id, id, site_id
+		
 	elsif doc.class.to_s == 'AacflmDirector'
 		puts doc.attributes['slug']	
 	else
