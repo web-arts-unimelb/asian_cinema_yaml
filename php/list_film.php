c5f47d7b3f78b1c2533537eb855f3161e4434bd8
arts-webmaster <web.arts.unimelb@gmail.com>
2014-03-22 21:45:45 +1100

    group_concat

--- php/list_film.php
+++ php/list_film.php
@@ -7,11 +7,19 @@ if(isset($_GET['film_id']) && !empty($_GET['film_id'])) {
 	
 	// http://stackoverflow.com/questions/12146905/inner-join-with-multiple-tables
 	// So use left join
+	
+	// http://stackoverflow.com/questions/8788344/concatenating-rows-in-relation-to-a-join
+	// GROUP_CONCAT example
+	
+	//http://stackoverflow.com/questions/3083499/mysql-distinct-on-a-group-concat
+	// GROUP_CONCAT has distinct
+	
 	$sql = "
 		SELECT
+			AacflmFilm.id AS film_id,
 			AacflmFilm.title AS film_title,
-			AacflmDirector.name as director_name,
-			AacflmProductionCompany.name as company_name,
+			GROUP_CONCAT(DISTINCT AacflmDirector.name SEPARATOR ', ') as director_name,
+			GROUP_CONCAT(DISTINCT AacflmProductionCompany.name SEPARATOR ', ') as company_name,
 			
 			AacflmFilm.year,
 			Category.name AS category,
@@ -48,6 +56,8 @@ if(isset($_GET['film_id']) && !empty($_GET['film_id'])) {
 		WHERE
 			AacflmFilm.id = %s AND
 			Tag.taggable_type	= 'AacflmFilm'
+		GROUP BY
+			AacflmFilm.id	
 	";
 	
 	
